7a13d02fe3b4d5a08d0bd9b285e4560ae2cf7852
Munachimso Ihionu <chisoihionu93@gmail.com>
2025-11-25 14:55:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -142,16 +142,16 @@ MongoClient.connect(db, (err, db) => {
     });
 
     // Insecure HTTP connection
-    http.createServer(app).listen(port, () => {
-        console.log(`Express http server listening on port ${port}`);
+    //http.createServer(app).listen(port, () => {
+        //console.log(`Express http server listening on port ${port}`);
     });
 
-    /*
+    
     // Fix for A6-Sensitive Data Exposure
     // Use secure HTTPS protocol
     https.createServer(httpsOptions, app).listen(port, () => {
         console.log(`Express http server listening on port ${port}`);
     });
-    */
+    
 
 });
